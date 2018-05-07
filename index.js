3249e2ec529786ca5eee265dcdda37b10e3aa824
Sean Clifford <sclifford6262@gmail.com>
2018-05-07 18:06:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ function BoardMember(name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
-  this.veto(){ return "No, I must disagree"}
-  this.approve(){ return "You can do that!"}
-  this.doCharity(){ return "I like to help people."}
-  this.releasePressStatement(){ return "You will see great things from Scuber."}
-  this.sayHi(){ return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`}
+  this.veto= function(){ return "No, I must disagree"}
+  this.approve= function(){ return "You can do that!"}
+  this.doCharity= function(){ return "I like to help people."}
+  this.releasePressStatement= function(){ return "You will see great things from Scuber."}
+  this.sayHi= function(){ return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`}
 }
